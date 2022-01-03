d700dc473f93b4f0e5957d309cc5758cf9f450d4
Petrovich-A <A.Piatrovich@gmail.com>
2022-01-03 15:30:40 +0300

    add ColumnNames

---
+++ src/main/java/by/petrovich/storage/dao/ColumnNames.java
@@ -0,0 +1,17 @@
+package by.petrovich.storage.dao;
+
+public class ColumnNames {
+    //users
+    public static final String USER_ID = "user_id";
+    public static final String USER_LOGIN_PERSONNEL_NUMBER = "login_personnel_number";
+    public static final String PASSWORD = "password";
+    public static final String EMPLOYEE_NAME = "employee_name";
+    public static final String EMPLOYEE_SURNAME = "employee_surname";
+    public static final String EMPLOYEE_PATRONYMIC = "employee_patronymic";
+    public static final String POSITION = "position";
+    public static final String EMAIL = "email";
+    public static final String CREATE_TIME = "create_time";
+    public static final String USER_ROLE_ID = "user_role_id";
+
+
+}
