fdcaadea6ff6a2d031739f0003211cec0ed8a28c
Petrovich-A <A.Piatrovich@gmail.com>
2022-01-04 09:34:52 +0300

    rewrite registration

--- src/main/java/by/petrovich/storage/controller/impl/Registration.java
+++ src/main/java/by/petrovich/storage/controller/impl/Registration.java
@@ -18,54 +18,43 @@ import java.util.Date;
 
 public class Registration implements Command {
     private static final Logger logger = LogManager.getLogger();
-    private final ServiceProvider SERVICE_PROVIDER = ServiceProvider.getInstance();
-    private final UserService USER_SERVICE = SERVICE_PROVIDER.getUserService();
-    private String loginPersonnelNumber;
-    private String password;
-    private String employeeName;
-    private String employeeSurname;
-    private String employeePatronymic;
-    private String position;
-    private String email;
+    private final ServiceProvider serviceProvider = ServiceProvider.getInstance();
+    private final UserService userService = serviceProvider.getUserService();
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        loginPersonnelNumber = request.getParameter("loginPersonnelNumber");
-        password = request.getParameter("password");
-        employeeName = request.getParameter("employeeName");
-        employeeSurname = request.getParameter("employeeSurname");
-        employeePatronymic = request.getParameter("employeePatronymic");
-        position = request.getParameter("position");
-        email = request.getParameter("email");
-
-        if (loginPersonnelNumber != null || password != null || employeeName != null
-                || employeeSurname != null || employeePatronymic != null || position != null || email != null) {
-            User user = buildUser(loginPersonnelNumber, password, employeeName, employeeSurname, employeePatronymic, position, email);
-            try {
-                USER_SERVICE.registration(user);
-                logger.log(Level.DEBUG, "user from UI is get", user.toString());
-                request.getSession(true).setAttribute("user from UI", user);
-                response.sendRedirect(PathToPage.LOG_IN);
-            } catch (ServiceException e) {
-                e.printStackTrace();
-            }
-        } else {
-            logger.log(Level.DEBUG, "user from UI form is empty");
+        User user = createUser(request);
+        try {
+            userService.registration(user);
+            logger.log(Level.DEBUG, "user from UI is get", user.toString());
+            request.getSession(true).setAttribute("user from UI", user);
+            response.sendRedirect(PathToPage.LOG_IN);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            logger.log(Level.ERROR, e.getLocalizedMessage());
         }
     }
 
-    private User buildUser(String loginPersonnelNumber, String password, String employeeName, String employeeSurname,
-                           String employeePatronymic, String position, String email) {
+    private User createUser(HttpServletRequest request) {
         User user = new User();
-        user.setLoginPersonnelNumber(Integer.parseInt(loginPersonnelNumber));
-        user.setPassword(password);
-        user.setEmployeeName(employeeName);
-        user.setEmployeeSurname(employeeSurname);
-        user.setEmployeePatronymic(employeePatronymic);
-        user.setPosition(position);
-        user.setEmail(email);
+        user.setLoginPersonnelNumber(Integer.parseInt(getParameterToCheck("loginPersonnelNumber", request)));
+        user.setPassword(getParameterToCheck("password", request));
+        user.setEmployeeName(getParameterToCheck("employeeName", request));
+        user.setEmployeeSurname(getParameterToCheck("employeeSurname", request));
+        user.setEmployeePatronymic(getParameterToCheck("employeePatronymic", request));
+        user.setPosition(getParameterToCheck("position", request));
+        user.setEmail(getParameterToCheck("email", request));
         user.setDate(new Date());
         user.setUserRole(UserRole.USER);
         return user;
+    }
+
+    private String getParameterToCheck(String name, HttpServletRequest request) {
+        final String parameter = request.getParameter(name);
+        if (parameter == null) {
+            throw new IllegalArgumentException("request have no parameter with name: " + name);
+        }
+        return parameter;
     }
 }
