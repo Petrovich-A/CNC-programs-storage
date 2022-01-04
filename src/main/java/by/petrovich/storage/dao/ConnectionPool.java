9cae1f7b5936e41d7831bdde8a24db1110ac9419
Petrovich-A <A.Piatrovich@gmail.com>
2022-01-04 14:56:25 +0300

    rewrite ConnectionPool, add ProxyConnection

--- src/main/java/by/petrovich/storage/dao/ConnectionPool.java
+++ src/main/java/by/petrovich/storage/dao/ConnectionPool.java
@@ -4,45 +4,92 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ConnectionPool {
-	private final static Logger logger = LogManager.getLogger();
+    private final static Logger logger = LogManager.getLogger();
+    private static final ReentrantLock reentrantLock = new ReentrantLock();
+    private static ConnectionPool instance;
+    private BlockingDeque<ProxyConnection> freeConnections;
+    private Queue<ProxyConnection> givenAwayConnections;
+    private static final int DEFAULT_POOL_SIZE = 12;
+    private static ProxyConnection proxyConnection;
 
-	private ConnectionPool() {
-	}
+    private ConnectionPool() {
+        freeConnections = new LinkedBlockingDeque<>(DEFAULT_POOL_SIZE);
+        givenAwayConnections = new ArrayDeque<>();
+    }
 
-	private static ConnectionPool instance = null;
+    public static ConnectionPool getInstance() {
+        if (instance == null) {
+            reentrantLock.lock();
+            if (instance == null) {
+                instance = new ConnectionPool();
+            }
+            reentrantLock.unlock();
+        }
+        return instance;
+    }
 
-	public static ConnectionPool getInstance() {
-		if (instance == null) {
-			instance = new ConnectionPool();
-		}
-		return instance;
-	}
+    public Connection getConnection() {
+        Connection connection;
+        try {
+            connection = freeConnections.take();
+            givenAwayConnections.offer(proxyConnection);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        InitialContext initialContext;
+        DataSource dataSource;
+        try {
+            initialContext = new InitialContext();
+            dataSource = (DataSource) initialContext.lookup("java:comp/env/jdbc/CncProgramPool");
+            connection = dataSource.getConnection();
+            logger.log(Level.DEBUG, "connection have been got", connection);
+        } catch (NamingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Naming problem", e);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Connection isn't available", e);
+        }
+        return connection;
+    }
 
-	public Connection getConnection() {
-		InitialContext initialContext = null;
-		DataSource dataSource = null;
-		Connection connection = null;
-		try {
-			initialContext = new InitialContext();
-			dataSource = (DataSource) initialContext.lookup("java:comp/env/jdbc/CncProgramPool"); 
-			connection = dataSource.getConnection();
-			logger.log(Level.DEBUG, "connection have been got", connection);
-		} catch (NamingException e) {
-//			throw new RuntimeException("Naming problem", e);
-			e.printStackTrace();
-		} catch (SQLException e) {
-//			throw new RuntimeException("Connection isn't available", e);
-			e.printStackTrace();
-		}
-		return connection;
-	}
+    public void releaseConnection(Connection connection) {
+        givenAwayConnections.remove(connection);
+        freeConnections.offer(proxyConnection);
+    }
+
+    public void destroyPool() {
+        for (int i = 0; i < DEFAULT_POOL_SIZE; i++) {
+            try {
+                freeConnections.take().reallyClose();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        deregisterDrivers();
+    }
+
+    private void deregisterDrivers() {
+        DriverManager.getDrivers().asIterator().forEachRemaining(driver -> {
+            try {
+                DriverManager.deregisterDriver(driver);
+            } catch (SQLException e) {
+                logger.log(Level.ERROR, "deregisterDrivers error", e);
+            }
+        });
+    }
 
 }
