d06eba6413131387b49fa3159debe386f2071e7e
Petrovich-A <A.Piatrovich@gmail.com>
2022-01-31 14:26:08 +0300

    add serialVersionUID

--- src/main/java/by/petrovich/storage/service/ServiceException.java
+++ src/main/java/by/petrovich/storage/service/ServiceException.java
@@ -1,23 +1,25 @@
 package by.petrovich.storage.service;
 
 public class ServiceException extends Exception {
-    public ServiceException() {
-        super();
-    }
+	private static final long serialVersionUID = 1L;
 
-    public ServiceException(String message) {
-        super(message);
-    }
+	public ServiceException() {
+		super();
+	}
 
-    public ServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	public ServiceException(String message) {
+		super(message);
+	}
 
-    public ServiceException(Throwable cause) {
-        super(cause);
-    }
+	public ServiceException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
-    protected ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
+	public ServiceException(Throwable cause) {
+		super(cause);
+	}
+
+	protected ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
 }
