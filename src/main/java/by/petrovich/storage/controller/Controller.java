07c363e841cf4ee24b2d808fd1eb64c0bcf83985
Petrovich-A <A.Piatrovich@gmail.com>
2022-03-03 10:50:31 +0300

    add logger and change default case

--- src/main/java/by/petrovich/storage/controller/Controller.java
+++ src/main/java/by/petrovich/storage/controller/Controller.java
@@ -2,9 +2,15 @@ package by.petrovich.storage.controller;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import by.petrovich.storage.controller.command.Command;
 import by.petrovich.storage.controller.command.CommandProvider;
+import by.petrovich.storage.controller.command.PathToPage;
 import by.petrovich.storage.controller.command.Router;
+import jakarta.security.auth.message.callback.PrivateKeyCallback.Request;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -13,7 +19,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LogManager.getLogger();
 	private static final String COMMAND_REQUEST_PARAM = "commandName";
+	private static final String COMMAND_REQUEST_ERROR = "Can't find required page.";
 	private final CommandProvider commandProvider = new CommandProvider();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -37,7 +45,9 @@ public class Controller extends HttpServlet {
 			break;
 
 		default:
-			break;
+			logger.log(Level.ERROR, "Can't find required page.");
+			request.setAttribute("error_message", COMMAND_REQUEST_ERROR);
+			request.getRequestDispatcher(PathToPage.ERROR).forward(request, response);
 		}
 	}
 
