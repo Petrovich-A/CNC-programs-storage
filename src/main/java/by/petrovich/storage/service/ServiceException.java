cc366bb5026cf6522e9aaefb1ad010e9da5fc735
Petrovich-A <A.Piatrovich@gmail.com>
2022-03-10 10:40:41 +0300

    add field message

--- src/main/java/by/petrovich/storage/service/ServiceException.java
+++ src/main/java/by/petrovich/storage/service/ServiceException.java
@@ -2,24 +2,26 @@ package by.petrovich.storage.service;
 
 public class ServiceException extends Exception {
 	private static final long serialVersionUID = 1L;
+	private String message = "message is empty";
 
 	public ServiceException() {
 		super();
 	}
 
+	public String getMessage() {
+		return message;
+	}
+
 	public ServiceException(String message) {
-		super(message);
+		this.message = message;
 	}
 
 	public ServiceException(String message, Throwable cause) {
-		super(message, cause);
+		super(cause);
+		this.message = message;
 	}
 
 	public ServiceException(Throwable cause) {
 		super(cause);
-	}
-
-	protected ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
 	}
 }
