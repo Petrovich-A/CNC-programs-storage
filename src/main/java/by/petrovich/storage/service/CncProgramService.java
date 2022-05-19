0c74047addba1c7c9b0deb8e6a23441246b177a5
Petrovich-A <A.Piatrovich@gmail.com>
2022-05-19 14:28:22 +0300

    add some comments

--- src/main/java/by/petrovich/storage/service/CncProgramService.java
+++ src/main/java/by/petrovich/storage/service/CncProgramService.java
@@ -7,37 +7,117 @@ import by.petrovich.storage.entity.CncMachine;
 import by.petrovich.storage.entity.CncProgram;
 import by.petrovich.storage.entity.Detail;
 
+/**
+ * @author Petrovich A.V.
+ *
+ */
 public interface CncProgramService {
+	/**
+	 * @return
+	 * @throws ServiceException
+	 */
 	List<Detail> readDetail() throws ServiceException;
 
+	/**
+	 * @param id
+	 * @return
+	 * @throws ServiceException
+	 */
 	Optional<CncProgram> readCncProgramById(int id) throws ServiceException;
 
+	/**
+	 * @return
+	 * @throws ServiceException
+	 */
 	List<CncMachine> readAllCncMachines() throws ServiceException;
 
+	/**
+	 * @param name
+	 * @return
+	 * @throws ServiceException
+	 */
 	CncProgram readCncProgramByName(String name) throws ServiceException;
 
+	/**
+	 * @param id
+	 * @return
+	 * @throws ServiceException
+	 */
 	Detail readDetailById(int id) throws ServiceException;
 
+	/**
+	 * @param id
+	 * @return
+	 * @throws ServiceException
+	 */
 	CncMachine readCncMachine(int id) throws ServiceException;
 
+	/**
+	 * @param offset
+	 * @param numberOfRecords
+	 * @return
+	 * @throws ServiceException
+	 */
 	List<CncProgram> receiveBatch(int offset, int numberOfRecords) throws ServiceException;
 
+	/**
+	 * @return
+	 * @throws ServiceException
+	 */
 	List<CncProgram> receiveBatchByDate() throws ServiceException;
 
+	/**
+	 * @param name
+	 * @return
+	 * @throws ServiceException
+	 */
 	List<CncProgram> receiveBatchByDetailName(String name) throws ServiceException;
 
+	/**
+	 * @param personnelNumber
+	 * @return
+	 * @throws ServiceException
+	 */
 	List<CncProgram> receiveBatchByPersonnelNumber(int personnelNumber) throws ServiceException;
 
+	/**
+	 * @return
+	 * @throws ServiceException
+	 */
 	int receiveNumberOfRecords() throws ServiceException;
 
+	/**
+	 * @param cncProgram
+	 * @throws ServiceException
+	 */
 	void createCncProgram(CncProgram cncProgram) throws ServiceException;
 
+	/**
+	 * @param ñncProgram
+	 * @param id
+	 * @throws ServiceException
+	 */
 	void updateCncProgram(CncProgram ñncProgram, int id) throws ServiceException;
 
+	/**
+	 * @param detail
+	 * @param id
+	 * @throws ServiceException
+	 */
 	void updateDetail(Detail detail, int id) throws ServiceException;
 
+	/**
+	 * @param cncMachine
+	 * @param id
+	 * @throws ServiceException
+	 */
 	void updateCncMachine(CncMachine cncMachine, int id) throws ServiceException;
 
+	/**
+	 * @param cncProgram
+	 * @return
+	 * @throws ServiceException
+	 */
 	boolean isCncProgramValid(CncProgram cncProgram) throws ServiceException;
 
 }
--- src/main/java/by/petrovich/storage/service/UserService.java
+++ src/main/java/by/petrovich/storage/service/UserService.java
@@ -6,27 +6,99 @@ import java.util.Optional;
 import by.petrovich.storage.entity.RegistrationUserInfo;
 import by.petrovich.storage.entity.User;
 
+/**
+ * @author Petrovich A.V.
+ *
+ */
 public interface UserService {
+	/**
+	 * @return
+	 * @throws ServiceException
+	 */
 	List<User> readAllUsers() throws ServiceException;
 
+	/**
+	 * @param personnelNumber
+	 * @return
+	 * @throws ServiceException
+	 */
 	Optional<User> readUserByPersonnelNumber(int personnelNumber) throws ServiceException;
 
+	/**
+	 * @param user
+	 * @param personnelNumber
+	 * @throws ServiceException
+	 */
 	void update(User user, int personnelNumber) throws ServiceException;
 
+	/**
+	 * @param registrationUserInfo
+	 * @throws ServiceException
+	 */
 	void registrateUser(RegistrationUserInfo registrationUserInfo) throws ServiceException;
 
+	/**
+	 * @param user
+	 * @throws ServiceException
+	 */
 	void logOut(User user) throws ServiceException;
 
+	/**
+	 * @param login
+	 * @param password
+	 * @return
+	 * @throws ServiceException
+	 */
 	Optional<User> authorizateUser(int login, String password) throws ServiceException;
 
+	/**
+	 * Checks is the transferred user exist in date base.
+	 * 
+	 * @param personnelNumber
+	 * @return authorizated user from date base
+	 * @throws ServiceException
+	 */
 	boolean isUserExist(int personnelNumber) throws ServiceException;
 
+	/**
+	 * Checks is registration information (data) from registration form valid.
+	 * 
+	 * @param registrationUserInfo
+	 * @return true when user exists in base date
+	 */
 	boolean isValid(RegistrationUserInfo registrationUserInfo);
 
+	/**
+	 * Checks is the transferred login and password from authorization form is match
+	 * with login and password from date base.
+	 * 
+	 * @param login
+	 * @param password
+	 * @return true when login and password match with login and password from date
+	 *         base
+	 * @throws ServiceException
+	 */
 	boolean isLoginAndPasswordMatchWithDateBaseData(int login, String password) throws ServiceException;
 
+	/**
+	 * Checks is the transferred login and password from authorization form valid.
+	 * 
+	 * @param login
+	 * @param password
+	 * @return true when both are valid
+	 * @throws ServiceException
+	 */
 	boolean isLoginAndPasswordValid(int login, String password) throws ServiceException;
 
+	/**
+	 * Checks is the the transferred login and password from authorization form
+	 * valid.
+	 * 
+	 * @param password
+	 * @param confirmPassword
+	 * @return true when login and password valid
+	 * @throws ServiceException
+	 */
 	boolean isPasswordsMatch(String password, String confirmPassword) throws ServiceException;
 
 }
